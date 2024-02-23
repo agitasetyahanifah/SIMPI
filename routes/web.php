5e37e42292d44549be8197989d1c09f611fbf843
agitasetyahanifah <92209642+agitasetyahanifah@users.noreply.github.com>
2024-02-23 11:33:12 +0700

    first commit

--- routes/web.php
+++ routes/web.php
@@ -14,12 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-
-// Route::get('/', function () {
-//     return view('Admin.ManajemenKeuangan.manajemenkeuangan');
-// });
-
-Route::get('/admin/manajemen-keuangan', [ManajemenKeuangan::class, 'index']);
+Route::get('/', function () {
+    return view('welcome');
+});
