f259b0b7360370626c473d8e4eff6c413d1f53b2
agitasetyahanifah <92209642+agitasetyahanifah@users.noreply.github.com>
2024-02-23 11:04:35 +0700

    first commit

---
+++ database/migrations/2024_02_23_032237_create_manajemen_keuangan.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('manajemen_keuangan', function (Blueprint $table) {
+            $table->id();
+            $table->date('tanggal_transaksi');
+            $table->double('jumlah');
+            $table->enum('jenis', ['Pemasukan', 'Pengeluaran']);
+            $table->string('keterangan')->nullable();
+            $table->timestamps(); // This will automatically create created_at and updated_at columns
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('manajemen_keuangan');
+    }
+};
