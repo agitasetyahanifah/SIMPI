c941db257a9342a0a43e2ca739a32e80f108d07c
agitasetyahanifah <92209642+agitasetyahanifah@users.noreply.github.com>
2024-02-28 14:01:55 +0700

    first commit

--- routes/web.php
+++ routes/web.php
@@ -14,8 +14,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
 Route::get('/', function () {
-    return view('welcome');
+    return view('Admin.Layouts.main1');
 });
 
 // Admin
