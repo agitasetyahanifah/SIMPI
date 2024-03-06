a94a8329209e8f29f947d5a960686ce91e0c7766
agitasetyahanifah <92209642+agitasetyahanifah@users.noreply.github.com>
2024-03-06 17:15:58 +0700

    first commit

--- resources/views/Admin/Dashboard/index.blade.php
+++ resources/views/Admin/Dashboard/index.blade.php
@@ -165,7 +165,7 @@
                                     </div>
                                     <div class="modal-footer">
                                         <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Batal</button>
-                                        <button type="button" class="btn btn-danger" id="confirmDelete">Hapus</button>
+                                        <button type="submit" class="btn btn-danger" id="confirmDelete">Hapus</button>
                                     </div>
                                 </div>
                             </div>
