59756a35f1242a2b4e158979abc640652504df9f
agitasetyahanifah <92209642+agitasetyahanifah@users.noreply.github.com>
2024-03-26 16:03:31 +0700

    first commit

--- resources/views/Admin/PengelolaanIkan/index.blade.php
+++ resources/views/Admin/PengelolaanIkan/index.blade.php
@@ -20,29 +20,32 @@
     </div>
 @endif
 
-<div class="container">
-    <ul class="nav nav-pills mb-3" role="tablist">
-        <li class="nav-item">
-            <button type="button" class="nav-link active" role="tab" data-bs-toggle="tab" data-bs-target="#navs-pills-top-home" aria-controls="navs-pills-top-home" aria-selected="true">Data Ikan Masuk</button>
-        </li>
-        <li class="nav-item">
-            <button type="button" class="nav-link" role="tab" data-bs-toggle="tab" data-bs-target="#navs-pills-top-profile" aria-controls="navs-pills-top-profile" aria-selected="false">Data Ikan Keluar</button>
-        </li>
-    </ul>
+<div class="container-fluid py-4">
+    <div class="row">
+      <div class="col-12">
+        <div class="card">
+          <div class="card-header pb-0">
+            <h5 class="font-weight-bolder mb-3">Manajemen Pengelolaan Ikan</h5>
 
-    <!-- Tab panes -->
-    <div class="tab-content">
-        <div class="tab-pane fade show active" id="navs-pills-top-home" role="tabpanel">
-            Content for Data Ikan Masuk
-        </div>
-        <div class="tab-pane fade" id="navs-pills-top-profile" role="tabpanel">
-            Content for Data Ikan Keluar
-        </div>
+            <div class="nav-wrapper position-relative end-0">
+                <ul class="nav nav-pills nav-fill p-1" role="tablist">
+                   <li class="nav-item">
+                      <a class="nav-link mb-0 px-0 py-1 active" data-bs-toggle="tab" href="#profile-tabs-simple" role="tab" aria-controls="profile" aria-selected="true">
+                      Pengelolaan Ikan Masuk
+                      </a>
+                   </li>
+                   <li class="nav-item">
+                      <a class="nav-link mb-0 px-0 py-1" data-bs-toggle="tab" href="#dashboard-tabs-simple" role="tab" aria-controls="dashboard" aria-selected="false">
+                      Pengelolaan Ikan Keluar
+                      </a>
+                   </li>
+                 </ul>
+             </div>
+
+          </div>
+        </div>  
+      </div>
     </div>
 </div>
-
-<!-- Bootstrap JS and dependencies -->
-<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js"></script>
 
 @endsection
