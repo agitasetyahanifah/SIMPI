16afb5be003542b790300496a84dfc93bf53390a
agitasetyahanifah <92209642+agitasetyahanifah@users.noreply.github.com>
2024-04-01 14:56:49 +0700

    first commit

--- resources/views/Admin/Blog/index.blade.php
+++ resources/views/Admin/Blog/index.blade.php
@@ -170,7 +170,7 @@
             {{-- Modal Detail Blog --}}
             @foreach ($blogs as $blog)
             <div class="modal fade" id="detailModal{{ $blog->id }}" tabindex="-1" role="dialog" aria-labelledby="detailModalLabel{{ $blog->id }}" aria-hidden="true">
-                <div class="modal-dialog modal-dialog-centered modal-xl" role="document">
+                <div class="modal-dialog modal-fullscreen" role="document">
                     <div class="modal-content">
                         <div class="modal-header">
                             <h5 class="modal-title" id="detailModalTitle">Detail Blog/Artikel Pemancingan</h5>
